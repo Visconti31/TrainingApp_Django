1ac078582f771d5a1fc83da795f4211062df1f0e
unknown <visconti.joao@gmail.com>
2023-01-28 19:59:56 +0000

    Start workout app on backend

--- backend/workout/views.py
+++ backend/workout/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET'])
+def getRoutes(request):
+
+    routes = [
+        {
+            'Endpoint': '/workouts/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Return an array of workouts'
+        },
+        {
+            'Endpoint': '/workouts/id',
+            'method': 'GET',
+            'body': None,
+            'description': 'Return a workout'
+        }
+    ]
+
+    return Response(routes)
